cc259a928f23ae4e36aaf12be81428df902a9907
nov8garimagupta <32295065+nov8garimagupta@users.noreply.github.com>
2018-02-28 15:14:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');//create a web server
 var morgan = require('morgan');//help us outut logs on server
 var path = require('path');
+var pool = require('pg').pool;
 
+var config={
+    user:'	nov8garimagupta',
+    database:'	nov8garimagupta',
+    host:'db.imad.hasura.app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -22,6 +30,18 @@ app.get('/ui/main.js', function (req, res) {
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+var pool=new Pool(config);
+app.get('/test.db',function (req,res){
+   //make a select request
+   //return a response with a result
+   pool.Query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500),send(err,toString());
+       }else{
+           res.send(JSON,stringify(result));
+       }
+   });
 });
 
 app.get('/ui/madi.png', function (req, res) {
